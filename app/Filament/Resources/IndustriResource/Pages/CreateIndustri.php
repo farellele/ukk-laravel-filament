a01185bcea8bb875e542b7719500921aa1c33aa3
farellele <farelkristiawan87@gmail.com>
2025-05-18 16:21:03 +0700

    (kata-kata)

--- app/Filament/Resources/IndustriResource/Pages/CreateIndustri.php
+++ app/Filament/Resources/IndustriResource/Pages/CreateIndustri.php
@@ -1,12 +1,27 @@
 <?php
-
 namespace App\Filament\Resources\IndustriResource\Pages;
 
 use App\Filament\Resources\IndustriResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
+use App\Models\Industri;
 
 class CreateIndustri extends CreateRecord
 {
     protected static string $resource = IndustriResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // Cek apakah nama industri sudah ada di database
+        if (Industri::where('nama', $data['nama'])->exists()) {
+            Notification::make()
+                ->title('Industri sudah ada.')
+                ->warning()
+                ->send();
+
+            $this->halt(); // Menghentikan proses tanpa menyimpan data baru
+        }
+
+        return $data;
+    }
 }
--- app/Filament/Resources/PKLResource/Pages/CreatePKL.php
+++ app/Filament/Resources/PKLResource/Pages/CreatePKL.php
@@ -1,12 +1,27 @@
 <?php
-
 namespace App\Filament\Resources\PKLResource\Pages;
 
 use App\Filament\Resources\PKLResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
+use App\Models\PKL;
 
 class CreatePKL extends CreateRecord
 {
     protected static string $resource = PKLResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // Cek apakah siswa sudah memiliki PKL
+        if (PKL::where('siswa_id', $data['siswa_id'])->exists()) {
+            Notification::make()
+                ->title('Siswa sudah mengisi PKL.')
+                ->warning()
+                ->send();
+
+            $this->halt(); // Menghentikan proses tanpa menyimpan data baru
+        }
+
+        return $data;
+    }
 }
